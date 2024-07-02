76eef3882e21ba366f02a9b0a68e768a7e2b9c48
jki12 <sjak0227@naver.com>
2024-07-02 15:44:01 +0900

    fix: Conditional 추가

--- src/main/java/com/nhnacademy/common/entity/BaseDateTimeEntity.java
+++ src/main/java/com/nhnacademy/common/entity/BaseDateTimeEntity.java
@@ -2,6 +2,7 @@ package com.nhnacademy.common.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@ConditionalOnProperty(value = "common.entity.enable", havingValue = "true")
 @Getter
 @Setter
 @MappedSuperclass
--- src/main/java/com/nhnacademy/common/entity/BaseEntity.java
+++ src/main/java/com/nhnacademy/common/entity/BaseEntity.java
@@ -2,12 +2,14 @@ package com.nhnacademy.common.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+@ConditionalOnProperty(value = "common.entity.enable", havingValue = "true")
 @Getter
 @Setter
 @MappedSuperclass
